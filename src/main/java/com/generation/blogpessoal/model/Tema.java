8c6186574a3cbed4d0048dadbb162d936f9c6ec9
Pablo Henrique <pablo.h.s.maia@gmail.com>
2023-12-12 16:33:05 -0300

    Criando Relacionamento entre tabelas

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,9 +1,16 @@
 package com.generation.blogpessoal.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,6 +27,20 @@ public class Tema {
 	@NotBlank(message = "O Atributo Descrição é obrigatório!")
 	@Size(max = 255, message = "O Atributo Descrição deve conter no mínimo 10 e no maximo 255 caractéres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 
 	public Long getId() {
